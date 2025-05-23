c671c0e6de55d4127aafb59a14fe08f63c617b0b
Sankharanarayanan.K <sankharworld@gmail.com>
2025-05-23 10:10:53 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL:"https://poultry-e-commerce-backend.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
